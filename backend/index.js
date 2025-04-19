dbd1d02cc58f9aa8d4d255bdf5e831048c3b0845
Abhay Awasthi <codewithabhay11@gmail.com>
2025-04-19 08:31:43 +0530

    fixing bad commits creating login signup routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './database/db.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import userRoute from './routes/user.route.js';
 dotenv.config();
 
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin : process.env.BACKEND_URL , 
+    credentials : true
+}))
+
+app.use('api/v1/user' , userRoute);
 
 app.listen(PORT , () => {
     console.log(`Server listening on port ${PORT}`);
